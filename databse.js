d852090a06020a038f6fb75407520313518a77ac
sanjay10985 <sanjaytomar717@gmail.com>
2022-12-10 16:58:19 +0530

    mongodb fix

--- databse.js
+++ databse.js
@@ -3,10 +3,9 @@ import dotenv from 'dotenv'
 
 dotenv.config();
 const mongoose_key = "mongodb+srv://admin:mMZ3nOD2UhRb4Y2D@cluster0.5o9l51r.mongodb.net/?retryWrites=true&w=majority";
-const connectToMongodb = () =>{
-mongoose.connect(mongoose_key,() => {
-    console.log("mongodb is been connected");
-})
+const connectToMongodb = async () =>{
+const conn = await mongoose.connect(mongoose_key);
+console.log('mongodb connected');
 }
 
 export default connectToMongodb;
